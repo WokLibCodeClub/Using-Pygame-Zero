8e7fe20edf0959635368596e35971033b2e61c8a
simonc8 <33280951+simonc8@users.noreply.github.com>
2021-01-26 17:53:53 +0000

    Update letterA1.py

--- letterA1.py
+++ letterA1.py
@@ -1,17 +1,21 @@
+import pgzrun
+
 # Set the size of the window by making numerical variables WIDTH and HEIGHT
 WIDTH = 640
 HEIGHT = 480
 
 # Make numerical variables x and y and give them initial values.
-x = WIDTH/2
-y = HEIGHT/2
+x_coord = WIDTH/2
+y_coord = HEIGHT/2
 
 
 # Define function update where the x coordinate will be changed
 def update():
-    global x
-    x = x + 1
+    global x_coord
+    x_coord += 1
 
 # Define function draw which will draw the letter on the screen
 def draw():
-    screen.draw.text('A', center=(x, y))
+    screen.draw.text('A', center=(x_coord, y_coord))
+
+pgzrun.go()
